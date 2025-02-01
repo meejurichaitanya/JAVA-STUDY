1d292ec14b1ee8a32a6bbbef3273acbad7c8b208
Chaitanya <meejurichaitanya@gmailcom>
2025-02-01 20:18:11 +0530

    new programs added

--- Leetcode/src/Special_array.java
+++ Leetcode/src/Special_array.java
@@ -2,7 +2,7 @@ public class Special_array {
     public static void main(String[] args){
 //https://leetcode.com/problems/special-array-i/?envType=daily-question&envId=2025-02-01
       int[] nums={1,2,3,4,5,6,7,8,9};
-      isArraySpecial(nums);
+      System.out.println(isArraySpecial(nums));
 
     }
 
